fc95f3922d3b77b6b810879199c326a492316664
a2ltprogramacion <a2lt.programacion@gmail.com>
2026-03-06 17:30:03 -0400

    feat: Implement Decap CMS configuration and update Astro site URL.

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   // 1. AGREGA ESTA LÍNEA (Vital para solucionar el error "Invalid URL")
-  site: 'https://a2lt-authority.netlify.app',
+  site: 'https://a2lt-authority-aesthetic.netlify.app',
 
   vite: {
     plugins: [tailwindcss()]
